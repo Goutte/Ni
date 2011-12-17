96e7c742cc779318ad89c0462fc0032cbc2c80ea
Goutte <antoine.goutenoir@gmail.com>
2011-12-17 13:11:28 +0100

    SubNav Concerts human readable date

--- apps/frontend/modules/aEvent/templates/_subNavConcerts.php
+++ apps/frontend/modules/aEvent/templates/_subNavConcerts.php
@@ -1,8 +1,12 @@
+<?php use_helper('Date') ?>
+
 <?php foreach ($concerts as $concert): ?>
 
 <h5>
-  <?php echo link_to($concert->getStartDate(), 'a_event_post', $concert) ?>
+  <?php echo link_to(format_date($concert->start_date, 'D'), 'a_event_post', $concert) ?>
 </h5>
 | <?php echo $concert->getFeedTitle() ?>
+
+<br />
 
 <?php endforeach ?>
