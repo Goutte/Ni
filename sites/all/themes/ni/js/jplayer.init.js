2295f945cc5b45417bb27da10215222a3c050d09
Kiouze <kiouze@gmail.com>
2012-03-09 15:44:50 +0100

    Intégration du player terminée

--- sites/all/themes/ni/js/jplayer.init.js
+++ sites/all/themes/ni/js/jplayer.init.js
@@ -1,16 +1,42 @@
 (function ($) {
 
 $(document).ready(function(){
-  $("#jquery_jplayer_1").jPlayer({
-    ready: function () {
-      $(this).jPlayer("setMedia", {
-        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
-      });
-    },
-    swfPath: "/js",
-    supplied: "m4a, oga"
-  });
+
+    new jPlayerPlaylist({
+        jPlayer: "#jquery_jplayer_1",
+        cssSelectorAncestor: "#jp_container_1"
+    }, [
+        {
+            title:"Cro Magnon Man",
+            mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+            oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
+        },
+        {
+            title:"Your Face",
+            mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+            oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+        },
+        {
+            title:"Cyber Sonnet",
+            mp3:"http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
+            oga:"http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
+        },
+        {
+            title:"Tempered Song",
+            mp3:"http://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
+            oga:"http://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg"
+        },
+        {
+            title:"Hidden",
+            mp3:"http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
+            oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
+        }
+    ], {
+        swfPath: "../js",
+        supplied: "oga, mp3",
+        wmode: "window"
+    });
+
 });
 
 })(jQuery);
--- sites/all/themes/ni/page.tpl.php
+++ sites/all/themes/ni/page.tpl.php
@@ -133,15 +133,14 @@
 
       <div id="jquery_jplayer_1" class="jp-jplayer"></div>
         <div id="jp_container_1" class="jp-audio">
-          <div class="jp-type-single">
+          <div class="jp-type-playlist">
             <div class="jp-gui jp-interface">
               <ul class="jp-controls">
                 <li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
                 <li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
                 <li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
-                <li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
-                <li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
-                <li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
+                <li><a href="javascript:;" class="jp-previous" tabindex="1">previous</a></li>
+                <li><a href="javascript:;" class="jp-next" tabindex="1">next</a></li>
               </ul>
               <div class="jp-progress">
                 <div class="jp-seek-bar">
@@ -154,15 +153,11 @@
               <div class="jp-time-holder">
                 <div class="jp-current-time"></div>
                 <div class="jp-duration"></div>
-                <ul class="jp-toggles">
-                  <li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
-                  <li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
-                </ul>
               </div>
             </div>
-            <div class="jp-title">
-              <ul>
-                <li>Bubble</li>
+            <div class="jp-playlist">
+              <ul style="display: block">
+                <li></li>
               </ul>
             </div>
             <div class="jp-no-solution">
