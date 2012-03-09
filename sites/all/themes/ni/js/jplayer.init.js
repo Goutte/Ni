27b95444b4b83fb61766db587b8fef26288e82fd
Kiouze <kiouze@gmail.com>
2012-03-09 13:56:04 +0100

    Intégration du player sans playlist

---
+++ sites/all/themes/ni/js/jplayer.init.js
@@ -0,0 +1,16 @@
+(function ($) {
+
+$(document).ready(function(){
+  $("#jquery_jplayer_1").jPlayer({
+    ready: function () {
+      $(this).jPlayer("setMedia", {
+        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+      });
+    },
+    swfPath: "/js",
+    supplied: "m4a, oga"
+  });
+});
+
+})(jQuery);
--- sites/all/themes/ni/page.tpl.php
+++ sites/all/themes/ni/page.tpl.php
@@ -1,5 +1,8 @@
 <?php
-  drupal_add_js('sites/all/themes/ni/ni.js');
+  drupal_add_js('sites/all/themes/ni/js/ni.js');
+  drupal_add_js('sites/all/themes/ni/js/jplayer.playlist.min.js');
+  drupal_add_js('sites/all/themes/ni/js/jquery.jplayer.min.js');
+  drupal_add_js('sites/all/themes/ni/js/jplayer.init.js');
 ?>
 
   <?php print render($page['header']); ?>
@@ -126,7 +129,50 @@
     </div>
 
     <div class="rgt_box_top" id="rgt_msc"></div>
-    <div class="rgt_box_body"></div>
+    <div class="rgt_box_body">
+
+      <div id="jquery_jplayer_1" class="jp-jplayer"></div>
+        <div id="jp_container_1" class="jp-audio">
+          <div class="jp-type-single">
+            <div class="jp-gui jp-interface">
+              <ul class="jp-controls">
+                <li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
+                <li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
+                <li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
+                <li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
+                <li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
+                <li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
+              </ul>
+              <div class="jp-progress">
+                <div class="jp-seek-bar">
+                  <div class="jp-play-bar"></div>
+                </div>
+              </div>
+              <div class="jp-volume-bar">
+                <div class="jp-volume-bar-value"></div>
+              </div>
+              <div class="jp-time-holder">
+                <div class="jp-current-time"></div>
+                <div class="jp-duration"></div>
+                <ul class="jp-toggles">
+                  <li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
+                  <li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="jp-title">
+              <ul>
+                <li>Bubble</li>
+              </ul>
+            </div>
+            <div class="jp-no-solution">
+              <span>Update Required</span>
+              To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
+            </div>
+          </div>
+        </div>
+
+    </div>
 
     <div class="rgt_box_top" id="rgt_scl"></div>
     <div class="rgt_box_body">
